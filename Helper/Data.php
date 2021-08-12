a8e6ce0026d6e94c6d19285b7f06f243f0a558c3
landofcoder <landofcoder@gmail.com>
2021-08-12 10:10:42 +0700

    updated files

---
+++ Helper/Data.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Venustheme
+ * 
+ * NOTICE OF LICENSE
+ * 
+ * This source file is subject to the Venustheme.com license that is
+ * available through the world-wide-web at this URL:
+ * http://www.venustheme.com/license-agreement.html
+ * 
+ * DISCLAIMER
+ * 
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ * 
+ * @category   Venustheme
+ * @package    Ves_ImageSlider
+ * @copyright  Copyright (c) 2014 Venustheme (http://www.venustheme.com/)
+ * @license    http://www.venustheme.com/LICENSE-1.0.html
+ */
+namespace Ves\ImageSlider\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+	/**
+     * @var \Magento\Cms\Model\Template\FilterProvider
+     */
+	protected $_filterProvider;
+
+	public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Cms\Model\Template\FilterProvider $filterProvider
+        ) {
+        parent::__construct($context);
+        $this->_filterProvider = $filterProvider;
+    }
+
+	public function filter($str)
+	{
+		$html = $this->_filterProvider->getPageFilter()->filter($str);
+		return $html;
+	}
+}
